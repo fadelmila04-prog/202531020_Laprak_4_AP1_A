7ceec1e07e780750a57846dc470274b9e5a6e7d2
fadelmila04-prog <fadelmila04@gmail.com>
2026-01-18 11:17:30 +0700

    Enhance attendance percentage output formatting
    Added iomanip header and setprecision for percentage output.

--- CekKehadiranKelas.cpp
+++ CekKehadiranKelas.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 int main() {
@@ -24,6 +25,7 @@ int main() {
 
     cout << "\nHadir: " << hadir << endl;
     cout << "Tidak hadir: " << tidakHadir << endl;
+    cout << fixed << setprecision(2);
     cout << "Persentase hadir: " << persentase << "%" << endl;
 
     return 0;
